b2b384fc38041483fe5873db9ad075e3713751b2
SkytecNZ <nznilan@gmail.com>
2025-06-08 18:47:33 +1200

    update to app.js

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
-console.log("Application is starting");
+console.log("My Node app is starting");
  
 const express = require('express')
 const app = express()
-const port = 3001
+const port = 3000
  
 app.get("/", (req, res, next) => {
     console.log("I received a get request on the path // ")
-    res.send('<h1 style="color:blue;">Acknowledged</h1>')
+    res.send('<h1 style="color:blue;">Hello World !!</h1>')
 })
  
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
  
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Node js app is listening on port ${port}`)
 })
